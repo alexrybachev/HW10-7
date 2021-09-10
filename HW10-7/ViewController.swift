57d60fec8e8b504d279eecf4ccc05f5b80cccb6f
Alex Rybachev <rybachev.as@gmail.com>
2021-09-10 08:01:38 +0400

    Initial Commit

---
+++ HW10-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10-7
+//
+//  Created by Aleksandr Rybachev on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
